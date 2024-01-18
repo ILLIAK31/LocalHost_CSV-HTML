4082ff2386d3b1e2d15ab9c0b2a7ef27c86ac0d0
Illia <karma@MYPC>
2024-01-18 17:31:58 +0100

    Add project files.

---
+++ LocalHost/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace LocalHost
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            port2 = new System.Windows.Forms.TextBox();
+            path = new System.Windows.Forms.TextBox();
+            button_port = new System.Windows.Forms.Button();
+            folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            button_folder = new System.Windows.Forms.Button();
+            button_stop = new System.Windows.Forms.Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label1.Location = new System.Drawing.Point(39, 34);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(94, 59);
+            label1.TabIndex = 0;
+            label1.Text = "Port";
+            // 
+            // label2
+            // 
+            label2.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label2.Location = new System.Drawing.Point(39, 188);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(94, 59);
+            label2.TabIndex = 1;
+            label2.Text = "Path";
+            // 
+            // port2
+            // 
+            port2.Location = new System.Drawing.Point(39, 111);
+            port2.Name = "port2";
+            port2.Size = new System.Drawing.Size(308, 27);
+            port2.TabIndex = 2;
+            // 
+            // path
+            // 
+            path.Location = new System.Drawing.Point(39, 296);
+            path.Name = "path";
+            path.Size = new System.Drawing.Size(308, 27);
+            path.TabIndex = 3;
+            // 
+            // button_port
+            // 
+            button_port.Location = new System.Drawing.Point(441, 49);
+            button_port.Name = "button_port";
+            button_port.Size = new System.Drawing.Size(266, 63);
+            button_port.TabIndex = 4;
+            button_port.Text = "Set Port";
+            button_port.UseVisualStyleBackColor = true;
+            button_port.Click += button_port_Click;
+            // 
+            // button_folder
+            // 
+            button_folder.Location = new System.Drawing.Point(353, 295);
+            button_folder.Name = "button_folder";
+            button_folder.Size = new System.Drawing.Size(40, 29);
+            button_folder.TabIndex = 5;
+            button_folder.Text = "...";
+            button_folder.UseVisualStyleBackColor = true;
+            button_folder.Click += button_folder_Click;
+            // 
+            // button_stop
+            // 
+            button_stop.Location = new System.Drawing.Point(441, 159);
+            button_stop.Name = "button_stop";
+            button_stop.Size = new System.Drawing.Size(266, 63);
+            button_stop.TabIndex = 6;
+            button_stop.Text = "Stop";
+            button_stop.UseVisualStyleBackColor = true;
+            button_stop.Click += button_stop_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(800, 450);
+            Controls.Add(button_stop);
+            Controls.Add(button_folder);
+            Controls.Add(button_port);
+            Controls.Add(path);
+            Controls.Add(port2);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Local Host";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox port2;
+        private System.Windows.Forms.TextBox path;
+        private System.Windows.Forms.Button button_port;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button_folder;
+        private System.Windows.Forms.Button button_stop;
+    }
+}
---
+++ LocalHost/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Threading;
+using System.Net.Sockets;
+using System.IO;
+
+namespace LocalHost
+{
+    public partial class Form1 : Form
+    {
+        private TcpListener tcpListener;
+        private Thread listenerThread;
+        private bool isServerRunning = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button_port_Click(object sender, EventArgs e)
+        {
+            if (!isServerRunning)
+            {
+                int port = int.Parse(port2.Text);
+                string directoryPath = path.Text;
+
+                // Convert CSV files to HTML files
+                ConvertCsvFilesToHtml(directoryPath);
+
+                // Start the server
+                listenerThread = new Thread(() => StartServer(port, directoryPath));
+                listenerThread.Start();
+
+                isServerRunning = true;
+                button_port.Enabled = false;
+                button_stop.Enabled = true;
+            }
+        }
+        private void ConvertCsvFilesToHtml(string directoryPath)
+        {
+            try
+            {
+                int fileNumber = 1;
+
+                foreach (string csvFilePath in Directory.EnumerateFiles(directoryPath, "*.csv"))
+                {
+                    // Read CSV content and convert it to HTML
+                    string csvContent = File.ReadAllText(csvFilePath);
+                    string htmlContent = ConvertCsvToHtml(csvContent);
+
+                    // Save the HTML content to a corresponding HTML file
+                    string htmlFileName = $"file{fileNumber}.html";
+                    string htmlFilePath = Path.Combine(directoryPath, htmlFileName);
+                    File.WriteAllText(htmlFilePath, htmlContent);
+
+                    fileNumber++;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error converting CSV to HTML: {ex.Message}");
+            }
+        }
+
+        private string ConvertCsvToHtml(string csvContent)
+        {
+            int i = 0;
+            string tag1 = "<th>", tag2 = "</th>", Text2 = "\n</table>\n</body>\n</html>", Text1 = "<!DOCTYPE html>\n<html>\n<head>\n<style>\ntable\n{\nfont-family: arial, sans-serif;\nborder-collapse: collapse;\nwidth: 100%;\n}\ntd, th\n{\nborder: 1px solid #dddddd;\ntext-align: left;\npadding: 8px;\n}\ntr:nth-child(even)\n{\nbackground-color: #c9c9c9;\n}\n</style>\n</head>\n<body>\n<table>";
+
+            foreach (var line1 in csvContent.Split('\n'))
+            {
+                Text1 += "\n<tr>\n";
+                var csv2 = line1.Split(';');
+                foreach (var line2 in csv2)
+                {
+                    Text1 += (tag1 + line2 + tag2);
+                }
+                Text1 += "\n</tr>";
+                if (i == 0)
+                {
+                    tag1 = "<td>";
+                    tag2 = "</td>";
+                }
+                ++i;
+            }
+
+            return Text1 + Text2;
+        }
+
+        private void button_stop_Click(object sender, EventArgs e)
+        {
+            if (isServerRunning)
+            {
+                tcpListener.Stop();
+                listenerThread.Join();
+
+                isServerRunning = false;
+                button_port.Enabled = true;
+                button_stop.Enabled = false;
+            }
+        }
+        private void StartServer(int port, string directoryPath)
+        {
+            try
+            {
+                tcpListener = new TcpListener(IPAddress.Any, port);
+                tcpListener.Start();
+
+                while (true)
+                {
+                    TcpClient client = tcpListener.AcceptTcpClient();
+                    Thread clientThread = new Thread(() => HandleClient(client, directoryPath));
+                    clientThread.Start();
+                }
+            }
+            catch (ThreadAbortException)
+            {
+                // Thread was aborted, likely due to stopping the server
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+
+        private void HandleClient(TcpClient tcpClient, string directoryPath)
+        {
+            try
+            {
+                using (NetworkStream networkStream = tcpClient.GetStream())
+                using (StreamReader reader = new StreamReader(networkStream))
+                using (StreamWriter writer = new StreamWriter(networkStream))
+                {
+                    string request = reader.ReadLine();
+                    string[] requestParts = request.Split(' ');
+
+                    if (requestParts.Length >= 2 && requestParts[0] == "GET")
+                    {
+                        string filePath = GetFilePath(directoryPath, requestParts[1]);
+                        string response = GenerateResponse(filePath);
+                        writer.Write(response);
+                    }
+                    else
+                    {
+                        writer.Write("HTTP/1.1 400 Bad Request\r\n\r\n");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error handling client: {ex.Message}");
+                SendErrorResponse(tcpClient.GetStream(), 500);
+            }
+            finally
+            {
+                tcpClient.Close();
+            }
+        }
+
+        private void SendErrorResponse(Stream stream, int statusCode)
+        {
+            StreamWriter writer = new StreamWriter(stream);
+            writer.Write($"HTTP/1.1 {statusCode} Internal Server Error\r\n\r\n");
+            writer.Flush();
+        }
+
+
+        private string GetFilePath(string directoryPath, string requestedPath)
+        {
+            if (string.IsNullOrEmpty(requestedPath) || requestedPath == "/")
+            {
+                // Serve default index.html
+                return Path.Combine(directoryPath, "index.html");
+            }
+
+            string fileName = requestedPath.TrimStart('/');
+            string filePath = Path.Combine(directoryPath, fileName);
+
+            if (File.Exists(filePath))
+            {
+                return filePath;
+            }
+            else
+            {
+                // If the requested file doesn't exist, serve the default 404.html
+                return Path.Combine(directoryPath, "404.html");
+            }
+        }
+
+
+        private string GenerateResponse(string filePath)
+        {
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    string fileContent = File.ReadAllText(filePath);
+
+                    return $"HTTP/1.1 200 OK\r\n" +
+                           $"Content-Length: {fileContent.Length}\r\n" +
+                           $"Content-Type: text/html; charset=utf-8\r\n\r\n" +
+                           $"{fileContent}";
+                }
+                else
+                {
+                    return "HTTP/1.1 404 Not Found\r\n\r\n";
+                }
+            }
+            catch
+            {
+                return "HTTP/1.1 500 Internal Server Error\r\n\r\n";
+            }
+        }
+
+
+        private void button_folder_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.SelectedPath = @"C:\";
+
+            // Show the folder browser dialog
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+
+            // Check if the user clicked OK
+            if (result == DialogResult.OK)
+            {
+                // Get the selected folder path
+                string selectedPath = folderBrowserDialog1.SelectedPath;
+
+                // Do something with the selected folder path
+                path.Text = selectedPath;
+            }
+        }
+    }
+}
